2e82e1765a0fce97fb422a453929e1531f92259c
eligifty-coder <gift.weike@gmail.com>
2025-07-11 11:39:35 +0100

    tutor live coding - props- passing data from parent to child component

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -2,40 +2,25 @@
 import UserList from './component/UserList'
 import ProductList from './component/ProductList'
 const App = () => {
-  const numbers =  [1,2,3,4,5]
-  const usersInfo = [
-        {
-          username: "HuXn",
-          email: "test@gmail.com",
-          location: "USA",
-        },
-        {
-          username: "John",
-          email: "jd@gmail.com",
-          location: "Arab",
-        },
-        {
-          username: "Alex",
-          email: "alexmersion@gmail.com",
-          location: "India",
-        },
-      ];
   return (
-    <main >
-      <h1>Rendering List in React</h1>
-      {
-        usersInfo.map(({email, username,  location})=>
-          <ul key= {Math.random()}>
-            <li>{username}</li>
-            <li>{email}</li>
-            <li>{location}</li>
-          </ul>
-        )
-      }
-      <UserList/>
-      <ProductList/>
-    </main>
+    <User
+    img ='https://avatars.githubusercontent.com/u/58071014?s=400&u=d76cf46c49a7bb6d83554935eeb4f10dd430f85a&v=4'
+    name='Gift_jsx' 
+    age= {22} 
+    isMarried={false} 
+    hobbies ={['Coding ', 'Reading ', 'Sleeping ']} />
   )
+}
+
+const User  = (props) =>{
+  console.log(props)
+  return <section>
+    <img src={props.img} alt={props.name} width='200' />
+    <h1>Name: {props.name}</h1>
+    <h2>Age: {props.age}</h2>
+    <h3>Is Married: { props.isMarried} </h3>
+    <h4>Hobbies: {props.hobbies}</h4>
+  </section>
 }
 
 export default App;
