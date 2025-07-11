522c280141a71c7cb37ec32880a40568364e0d24
eligifty-coder <gift.weike@gmail.com>
2025-07-11 11:04:00 +0100

    tutor live coding - mapping list of data

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,15 +1,34 @@
 
 const App = () => {
   const numbers =  [1,2,3,4,5]
+  const usersInfo = [
+        {
+          username: "HuXn",
+          email: "test@gmail.com",
+          location: "USA",
+        },
+        {
+          username: "John",
+          email: "jd@gmail.com",
+          location: "Arab",
+        },
+        {
+          username: "Alex",
+          email: "alexmersion@gmail.com",
+          location: "India",
+        },
+      ];
   return (
     <main >
       <h1>Rendering List in React</h1>
       {
-        numbers.map((num)=>{
-          return <ul>
-            <li key={Math.random()* numbers.length}>{num}</li>
+        usersInfo.map(({email, username,  location})=>
+          <ul key= {Math.random()}>
+            <li>{username}</li>
+            <li>{email}</li>
+            <li>{location}</li>
           </ul>
-        })
+        )
       }
     </main>
   )
