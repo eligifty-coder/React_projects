621184a26d5fe20695a01a128c3863d979503ef7
eligifty-coder <gift.weike@gmail.com>
2025-07-11 19:56:46 +0100

    ternary operator

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,15 +1,10 @@
 const InvalidPassword = () => <h1>Invalid Password</h1>
 const ValidPassword = () => <h1>Valid Password</h1>
 
-const Password = ({isValid}) => {
-    if(isValid){
-      console.log(isValid)
-        return <ValidPassword/>
-    }
-    return <InvalidPassword/>
-}
+const Password = ({isValid}) => isValid?  <ValidPassword/> :<InvalidPassword/>
+
 const App = () => {
-  return <Password isValid={false}/>
+  return <Password isValid={true}/>
 }
 
 
---
+++ react-demos/src/component/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+    const  items = ["Wireless Earbuds", "Power Bank", "New SSD", "Hoddie"];
+
+    return (
+    <div>Cart</div>
+    )
+}
+
+export default Cart
